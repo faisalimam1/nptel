d31ad96a761bf7df51e123b8c9bb78d1d5d368cb
Faisal Imam <163755226+faisalimam1@users.noreply.github.com>
2024-03-19 12:46:24 +0530

    Create ass5.java

---
+++ java/week 9/ass5.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class W09_P5 {
+
+    public static void main(String[] args) {
+        int a, b;
+        Scanner input = new Scanner(System.in);
+        // Read any two values for a and b.
+        // Get the result of a/b;
+        int result;
+
+        a = input.nextInt();
+        b = input.nextInt();
+        input.close();
+        // try block to divide two numbers and display the result
+        try {
+            result = a / b;
+            System.out.println(result);
+        }
+      //from here
+  
+
+    catch (ArithmeticException e) {
+            System.out.print("Cannot Divide by ZERO");
+        }
+    
+//to here
+
+  catch (Exception e) {
+            System.out.println("Exception Occoured");
+}
+}
+}
