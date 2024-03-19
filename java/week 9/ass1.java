21de1631926047ea6d1e82953d3f28714250f96c
Faisal Imam <163755226+faisalimam1@users.noreply.github.com>
2024-03-19 12:14:28 +0530

    Update ass1.java

--- java/week 9/ass1.java
+++ java/week 9/ass1.java
@@ -1,1 +1,18 @@
+    public SquareThread(int begin, int end) {
+        this.begin = begin;
+        this.end = end;
+    }
+
+    @Override
+    public synchronized void run() {
+        if (begin <= end) {
+            for (int i = begin; i <= end; i++) {
+                System.out.println(i * i);
+            }
+        } else {
+            for (int i = begin; i >= end; i--) {
+                System.out.println (i * i);
+            }
+        }
+    }
 
